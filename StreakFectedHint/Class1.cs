2172d90a9cb6a9035c0cc04dac36c6069838b364
Slvr99 <bsawii@gmail.com>
2019-04-27 20:01:47 -0400

    Add project files.

---
+++ StreakFectedHint/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using InfinityScript;
+
+public unsafe class SFHint : BaseScript
+{
+    public int CPOffset = 0x64813AF;
+    public SFHint()
+    {
+        *(int*)CPOffset = 0x16;
+        PlayerConnected += CustomWeapon_PlayerConnected;
+    }
+
+    void CustomWeapon_PlayerConnected(Entity obj)
+    {
+        obj.SpawnedPlayer += () => OnPlayerSpawned(obj);
+    }
+    public void OnPlayerSpawned(Entity player)
+    {
+        if (player.IsAlive && player.SessionTeam == "axis")
+        {
+            player.IPrintLnBold("^2Get kills using Martyrdom!");
+        }
+    }
+}
+
